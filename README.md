20aa4d0cc2923b92e145a3155797c4cc0d680544
bessonniy11 <50315006+bessonniy11@users.noreply.github.com>
2023-04-02 20:10:47 +0300

    0 i firebase & add environments

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # simple-auth
+
+0. Создание проекта в https://console.firebase.google.com/
+
+1. Ининциализация проекта.
+
+2. Установка firebase: npm install firebase @angular/fire
+
+Импорт нужных файлов в app.module.ts
+Добавление конфигурации firebase в файлы environments.ts .
+
+3. Создание необходимых компонентов.
+
+4. Создание маршрутизации.
+
+
---
+++ simple-auth/src/environments/environments.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyACxrlwxdWXioErGID10cbSEioM_939w8Y",
+    authDomain: "simply-authtorization.firebaseapp.com",
+    databaseURL: "simply-authtorization.firebaseapp.com",
+    projectId: "simply-authtorization",
+    storageBucket: "simply-authtorization.appspot.com",
+    messagingSenderId: "365317111320",
+    appId: "1:365317111320:web:a0cc5de2f215ed75e10d6d",
+    measurementId: "G-4269G3GT9J"
+  }
+};
